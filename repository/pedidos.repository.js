00a5ad92c0485284c9e8e9b37f4f40a50ecdfa1f
Sebastian Erives <serivesmejia@deltacv.org>
2026-02-07 01:21:09 -0600

    Añadir comentarios

--- repository/pedidos.repository.js
+++ repository/pedidos.repository.js
@@ -9,20 +9,20 @@ class PedidosRepository {
   }
 
   findById(id) {
-    return this.pedidos.find(p => p.id === id);
+    return this.pedidos.find(p => p.id === id); // Busca el pedido por su ID
   }
 
   create(pedido) {
     pedido.id = this.currentId++;
-    this.pedidos.push(pedido);
+    this.pedidos.push(pedido); // Agrega el nuevo pedido al array
     return pedido;
   }
 
   update(id, data) {
     const index = this.pedidos.findIndex(p => p.id === id);
     if (index === -1) return null;
-
-    this.pedidos[index] = { ...this.pedidos[index], ...data };
+ 
+    this.pedidos[index] = { ...this.pedidos[index], ...data }; // Solo actualizamos los campos proporcionados
     return this.pedidos[index];
   }
 
@@ -30,7 +30,7 @@ class PedidosRepository {
     const index = this.pedidos.findIndex(p => p.id === id);
     if (index === -1) return false;
 
-    this.pedidos.splice(index, 1);
+    this.pedidos.splice(index, 1); // Elimina el pedido del array
     return true;
   }
 }
